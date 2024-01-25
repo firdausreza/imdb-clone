7c7af02df474fd46bd694b045096bb86f5e03aea
Reza Firdaus <reza.firdaus@gramedia.id>
2024-01-25 20:11:18 +0700

    fix: delete unnecessary API functions

--- src/helpers/tmdb-api.js
+++ src/helpers/tmdb-api.js
@@ -13,54 +13,6 @@ export const tmdb = {
 			},
 		});
 	},
-	getNowPlayingMovies: () => {
-		return axios.get(`https://api.themoviedb.org/3/movie/now_playing`, {
-			headers: {
-				Accept: "application/json",
-			},
-			params: {
-				api_key: process.env.REACT_APP_TMDB_KEY,
-				language: "en-US",
-				page: 1,
-			},
-		});
-	},
-	getPopularMovies: () => {
-		return axios.get("https://api.themoviedb.org/3/movie/popular", {
-			headers: {
-				Accept: "application/json",
-			},
-			params: {
-				api_key: process.env.REACT_APP_TMDB_KEY,
-				language: "en-US",
-				page: 1,
-			},
-		});
-	},
-	getTopRatedMovies: () => {
-		return axios.get("https://api.themoviedb.org/3/movie/top_rated", {
-			headers: {
-				Accept: "application/json",
-			},
-			params: {
-				api_key: process.env.REACT_APP_TMDB_KEY,
-				language: "en-US",
-				page: 1,
-			},
-		});
-	},
-	getUpcomingMovies: () => {
-		return axios.get("https://api.themoviedb.org/3/movie/upcoming", {
-			headers: {
-				Accept: "application/json",
-			},
-			params: {
-				api_key: process.env.REACT_APP_TMDB_KEY,
-				language: "en-US",
-				page: 1,
-			},
-		});
-	},
 	getGenres: () => {
 		return axios.get("https://api.themoviedb.org/3/genre/movie/list", {
 			headers: {
